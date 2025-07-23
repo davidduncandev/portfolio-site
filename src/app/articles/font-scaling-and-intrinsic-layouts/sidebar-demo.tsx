f206091f007413c877170f064cc395fe79e0d9ed
David Duncan <david.duncan@sky.uk>
2025-07-23 08:13:42 +0100

    adjusting layout demo styles for light mode

--- src/app/articles/font-scaling-and-intrinsic-layouts/sidebar-demo.tsx
+++ src/app/articles/font-scaling-and-intrinsic-layouts/sidebar-demo.tsx
@@ -35,37 +35,37 @@ export function ComplexLayoutDemo() {
   return (
     <AnimateContainer name="Complex Layout">
       <div className="grid grid-cols-[repeat(auto-fit,minmax(min(var(--ui-grid-min,12rem),100%),1fr))] gap-4">
-        <div className="border-base-900 border-4 border-zinc-300 p-4 dark:text-white">
+        <div className="border-2 border-zinc-400 p-4 dark:text-white">
           <p className="my-2">SideBar</p>
           <div className="flex flex-wrap gap-4">
-            <div className="border-base-900 grid grow basis-12 place-content-center border-2 border-zinc-300 p-6 dark:text-white"></div>
-            <div className="border-base-900 grid min-w-[60%] grow-[999] basis-0 border-2 border-zinc-300 p-4 dark:text-white">
+            <div className="grid grow basis-12 place-content-center border-2 border-zinc-400 p-6 dark:text-white"></div>
+            <div className="grid min-w-[60%] grow-[999] basis-0 border-2 border-zinc-400 p-4 dark:text-white">
               <p className="my-2">Cluster</p>
               <div className="flex flex-wrap gap-2">
-                <div className="border-base-900 border border-zinc-300 p-4 px-2 dark:text-white"></div>
-                <div className="border-base-900 border border-zinc-300 p-4 dark:text-white"></div>
-                <div className="border-base-900 border border-zinc-300 p-4 px-6 dark:text-white"></div>
-                <div className="border-base-900 border border-zinc-300 p-4 px-6 dark:text-white"></div>
+                <div className="border border-zinc-400 p-4 px-2 dark:text-white"></div>
+                <div className="border border-zinc-400 p-4 dark:text-white"></div>
+                <div className="border border-zinc-400 p-4 px-6 dark:text-white"></div>
+                <div className="border border-zinc-400 p-4 px-6 dark:text-white"></div>
               </div>
             </div>
           </div>
         </div>
-        <div className="border-base-900 border-4 border-zinc-300 p-4 dark:text-white">
+        <div className="border-2 border-zinc-400 p-4 dark:text-white">
           <p className="m-2">Grid</p>
           <div className="grid grid-cols-[repeat(auto-fit,minmax(min(var(--ui-grid-min,6rem),100%),1fr))] gap-4">
-            <div className="border-base-900 border-4 border-zinc-300 p-2 dark:text-white">
+            <div className="border-2 border-zinc-400 p-2 dark:text-white">
               <p className="my-2">Switcher</p>
               <div className="flex flex-wrap gap-4 [&>*]:basis-[calc((var(--ui-switcher-threshold,8.5rem)-100%)*999)]">
-                <div className="border-base-900 border border-zinc-300 p-4 dark:text-white"></div>
-                <div className="border-base-900 border border-zinc-300 p-4 dark:text-white"></div>
-                <div className="border-base-900 border border-zinc-300 p-4 dark:text-white"></div>
+                <div className="border border-zinc-400 p-4 dark:text-white"></div>
+                <div className="border border-zinc-400 p-4 dark:text-white"></div>
+                <div className="border border-zinc-400 p-4 dark:text-white"></div>
               </div>
             </div>
-            <div className="border-base-900 border-4 border-zinc-300 p-6 dark:text-white">
+            <div className="border-2 border-zinc-400 p-6 dark:text-white">
               <p className="my-2">Grid</p>
               <div className="grid grid-cols-[repeat(auto-fit,minmax(min(var(--ui-grid-min,4rem),100%),1fr))] gap-4">
-                <div className="border-base-900 border border-zinc-300 p-6 dark:text-white" />
-                <div className="border-base-900 border border-zinc-300 p-6 dark:text-white" />
+                <div className="border border-zinc-400 p-6 dark:text-white" />
+                <div className="border border-zinc-400 p-6 dark:text-white" />
               </div>
             </div>
           </div>
